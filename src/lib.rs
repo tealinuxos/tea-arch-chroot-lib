a37ba396e30f59665976f19de9189d64a0d728ad
gagahsyuja <gghssa1@gmail.com>
2024-06-12 20:53:04 +0700

    Cleaning up

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
 pub mod prechroot;
 pub mod chroot;
 pub mod resource;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
