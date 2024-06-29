86b387758ae464160b3ca611510d3c33835765a3
gagahsyuja <gghssa1@gmail.com>
2024-06-30 03:54:45 +0700

    Preserve locale.gen and use main locale only

--- src/chroot/locale.rs
+++ src/chroot/locale.rs
@@ -1,34 +1,35 @@
 use std::fs::File;
 use duct::cmd;
-use std::io::{ Write, Error };
+use std::io::{ Write, Read, BufWriter, Error, BufReader  };
 use serde::{ Serialize, Deserialize };
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Locale
 {
     main: String,
-    locales: Vec<String>
+    // locales: Vec<String>
 }
 
 impl Locale
 {
-    pub fn new(main: &str, locales: Vec<&str>) -> Self
+    // pub fn new(main: &str, locales: Vec<&str>) -> Self
+    pub fn new(main: &str) -> Self
     {
         Self
         {
             main: main.to_string(),
-            locales: locales.iter().map(|s| s.to_string()).collect::<Vec<String>>()
+            // locales: locales.iter().map(|s| s.to_string()).collect::<Vec<String>>()
         }
     }
 
     pub fn set_locale(self) -> Result<(), Error>
     {
-        match Self::write_locale(self.locales)
+        match Self::write_locale(&self.main)
         {
             Ok(_) => {
 
                 cmd!("arch-chroot", "/mnt", "locale-gen").run()?;
-                Self::set_lang_variable(self.main)?;
+                Self::set_lang_variable(&self.main)?;
 
                 Ok(())
             }
@@ -37,23 +38,45 @@ impl Locale
         }
     }
 
-    fn write_locale(locales: Vec<String>) -> Result<(), Error>
+    // fn write_locale(locales: Vec<String>) -> Result<(), Error>
+    // {
+    //     let locales = locales
+    //         .iter()
+    //         .map(|s| s.to_string() + "\n")
+    //         .collect::<String>();
+    //
+    //     let locale_dot_gen = File::create("/mnt/etc/locale.gen");
+    //
+    //     match locale_dot_gen
+    //     {
+    //         Ok(mut file) => file.write_fmt(format_args!("{}", locales)),
+    //         Err(e) => Err(e)
+    //     }
+    // }
+
+    fn write_locale(locale: &str) -> Result<(), Error>
     {
-        let locales = locales
-            .iter()
-            .map(|s| s.to_string() + "\n")
+        let locale_dot_gen = File::open("/mnt/etc/locale.gen")?;
+        let mut locale_dot_gen = BufReader::new(locale_dot_gen);
+
+        let mut before = String::new();
+
+        locale_dot_gen.read_to_string(&mut before)?;
+
+        let after = before
+            .lines()
+            .map(|line| if line.ends_with(locale) { format!("{}\n", line.replace("#", "")) } else { format!("{}\n", line) })
             .collect::<String>();
 
-        let locale_dot_gen = File::create("/mnt/etc/locale.gen");
+        let locale_dot_gen = File::create("/mnt/etc/locale.gen")?;
+        let mut locale_dot_gen = BufWriter::new(locale_dot_gen);
 
-        match locale_dot_gen
-        {
-            Ok(mut file) => file.write_fmt(format_args!("{}", locales)),
-            Err(e) => Err(e)
-        }
+        locale_dot_gen.write_fmt(format_args!("{}", after))?;
+
+        Ok(())
     }
 
-    fn set_lang_variable(locale: String) -> Result<(), Error>
+    fn set_lang_variable(locale: &str) -> Result<(), Error>
     {
         let locale = locale.split_whitespace().next().unwrap();
         let locale_dot_conf = File::create("/mnt/etc/locale.conf");
