bac7f6e7cf397f1815bc091da0ffeaad96da1a7e
gagahsyuja <gghssa1@gmail.com>
2024-07-01 17:39:39 +0700

    Make struct derive serialize and deserialize

--- src/resource/mod.rs
+++ src/resource/mod.rs
@@ -1,3 +1,4 @@
+use serde::{ Serialize, Deserialize };
 pub mod timezone;
 pub mod locale;
 pub mod keyboard;
@@ -6,6 +7,7 @@ pub use self::timezone::Timezones;
 pub use self::locale::Locales;
 pub use self::keyboard::Keyboard;
 
+#[derive(Serialize, Deserialize)]
 pub enum FirmwareKind
 {
     UEFI,
